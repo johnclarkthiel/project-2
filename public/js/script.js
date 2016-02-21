e0aa26a7d68aff80f26f20ef7c0287a90647608f
John <johnclarkthiel@Johns-MBP.home>
2016-02-20 22:02:15 -0500

    attempting to add ability to publish, have not succeeded yet

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+console.log('HELLO!');
+$(function() {
+
+var $published = $('#publish').val();
+// console.log("IS IT PUBLISHED ... " + $published);
+
+// var $publishing = $('#publishing');
+// console.log($publishing);
+
+// $publishing.click(function(){
+// 	$published = true;
+// 	console.log("IS IT PUBLISHED ... " + $published);
+// })
+
+
+}); //<<<END function on load
